0eece8e0eee4d07a7bd7429bf467b5c772e5e608
gabystern <stern.gabrielle@gmail.com>
2017-06-26 21:24:29 -0400

    added 100 more tweets for Tuesday

--- app/services/tweets_processor.rb
+++ app/services/tweets_processor.rb
@@ -10,7 +10,7 @@ class TweetsProcessor
   end
 
   def call_twitter
-    results = twitter_login.search("#travelban -rt", {language: "en", include_rts: false}).take(5)
+    results = twitter_login.search("#travelban -rt", {language: "en", include_rts: false}).take(100)
     results
   end
 
